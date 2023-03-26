3f6d0577ee7bc64629f0462482d70fd71b8e9b52
Daiki Ko <dk.kunifu@gmail.com>
2023-03-26 16:25:12 +0900

    buildフォルダの追加

---
+++ precache-manifest.a09a2f9c7b28050b4d1853d76cda42a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccbad9364880c8ddac47",
+    "url": "/static/css/main.0e6de9e1.chunk.css"
+  },
+  {
+    "revision": "ccbad9364880c8ddac47",
+    "url": "/static/js/main.ccbad936.chunk.js"
+  },
+  {
+    "revision": "f99a61411f9578bd120b",
+    "url": "/static/js/1.f99a6141.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3c1ca0ca6504e1e5691eb2dae1d2b116",
+    "url": "/index.html"
+  }
+];
